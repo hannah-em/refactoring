65c7cd054c1b2f43e8af1052499fe6d51ea97d52
hannah-em <aarfah210404@gmail.com>
2025-11-18 18:21:57 -0500

    Task 2.4

--- src/main/java/theater/StatementPrinter.java
+++ src/main/java/theater/StatementPrinter.java
@@ -1,7 +1,5 @@
 package theater;
 
-import static theater.Constants.*;
-
 import java.text.NumberFormat;
 import java.util.Locale;
 import java.util.Map;
@@ -10,6 +8,30 @@ import java.util.Map;
  * This class generates a statement for a given invoice of performances.
  */
 public class StatementPrinter {
+    public static final int BASE_VOLUME_CREDIT_THRESHOLD = 30;
+    public static final int COMEDY_EXTRA_VOLUME_FACTOR = 5;
+    // comedy amount constants
+    public static final int COMEDY_AMOUNT_PER_AUDIENCE = 300;
+    public static final int COMEDY_AUDIENCE_THRESHOLD = 20;
+    public static final int COMEDY_BASE_AMOUNT = 30000;
+    public static final int COMEDY_OVER_BASE_CAPACITY_AMOUNT = 10000;
+    public static final int COMEDY_OVER_BASE_CAPACITY_PER_PERSON = 500;
+    // tragedy amount constants
+    public static final int TRAGEDY_AUDIENCE_THRESHOLD = 30;
+    public static final int TRAGEDY_BASE_AMOUNT = 40000;
+    public static final int TRAGEDY_OVER_BASE = 1000;
+    // formatting constants
+    public static final int PERCENT_FACTOR = 100;
+    // history constants
+    public static final int HISTORY_BASE_AMOUNT = 20000;
+    public static final int HISTORY_OVER_BASE_CAPACITY_PER_PERSON = 1000;
+    public static final int HISTORY_AUDIENCE_THRESHOLD = 20;
+    public static final int HISTORY_VOLUME_CREDIT_THRESHOLD = 20;
+    // pastoral constants
+    public static final int PASTORAL_BASE_AMOUNT = 40000;
+    public static final int PASTORAL_OVER_BASE_CAPACITY_PER_PERSON = 2500;
+    public static final int PASTORAL_AUDIENCE_THRESHOLD = 20;
+    public static final int PASTORAL_VOLUME_CREDIT_THRESHOLD = 20;
     private final Invoice invoice;
     private final Map<String, Play> plays;
 
@@ -24,18 +46,18 @@ public class StatementPrinter {
      * @throws RuntimeException if one of the play types is not known
      */
     public String statement() {
-        final StringBuilder result = new StringBuilder("Statement for " + invoice.getCustomer() + System.lineSeparator());
+        final StringBuilder rlt = new StringBuilder("Statement for " + invoice.getCustomer() + System.lineSeparator());
 
         for (Performance performance : invoice.getPerformances()) {
             final String playName = getPlay(performance).getName();
             final int audience = performance.getAudience();
             final String frmt2 = usd(getAmount(performance));
-            result.append(String.format("  %s: %s (%s seats)%n", playName, frmt2, audience));
+            rlt.append(String.format("  %s: %s (%s seats)%n", playName, frmt2, audience));
         }
 
-        result.append(String.format("Amount owed is %s%n", usd(getTotalAmount())));
-        result.append(String.format("You earned %s credits%n", getTotalVolumeCredits()));
-        return result.toString();
+        rlt.append(String.format("Amount owed is %s%n", usd(getTotalAmount())));
+        rlt.append(String.format("You earned %s credits%n", getVolumeCredits()));
+        return rlt.toString();
     }
 
     private int getTotalAmount() {
@@ -46,16 +68,16 @@ public class StatementPrinter {
         return totalAmount;
     }
 
-    private int getTotalVolumeCredits() {
+    private int getVolumeCredits() {
         int volumeCredits = 0;
         for (Performance performance : invoice.getPerformances()) {
 
             // add volume credits
 
-            volumeCredits += Math.max(performance.getAudience() - Constants.BASE_VOLUME_CREDIT_THRESHOLD, 0);
+            volumeCredits += Math.max(performance.getAudience() - BASE_VOLUME_CREDIT_THRESHOLD, 0);
             // add extra credit for every five comedy attendees
             if ("comedy".equals(getPlay(performance).getType())) {
-                volumeCredits += performance.getAudience() / Constants.COMEDY_EXTRA_VOLUME_FACTOR;
+                volumeCredits += performance.getAudience() / COMEDY_EXTRA_VOLUME_FACTOR;
             }
         }
         return volumeCredits;
@@ -76,11 +98,11 @@ public class StatementPrinter {
             case "tragedy":
                 thisAmount = TRAGEDY_BASE_AMOUNT;
                 if (performance.getAudience() > TRAGEDY_AUDIENCE_THRESHOLD) {
-                    thisAmount += TRAGEDY_OVER_BASE_CAPACITY_PER_PERSON * (performance.getAudience() - TRAGEDY_AUDIENCE_THRESHOLD);
+                    thisAmount += TRAGEDY_OVER_BASE * (performance.getAudience() - TRAGEDY_AUDIENCE_THRESHOLD);
                 }
                 break;
             case "comedy":
-                thisAmount = Constants.COMEDY_BASE_AMOUNT;
+                thisAmount = COMEDY_BASE_AMOUNT;
                 if (performance.getAudience() > Constants.COMEDY_AUDIENCE_THRESHOLD) {
                     thisAmount += Constants.COMEDY_OVER_BASE_CAPACITY_AMOUNT
                             + (Constants.COMEDY_OVER_BASE_CAPACITY_PER_PERSON
